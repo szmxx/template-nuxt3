277834b3b19299edf21dac09da2d3ca8ea259d2e
cola <szmintel@gmail.com>
2023-09-10 21:07:27 +0800

    Revert "refactor(core): remote i18n to single branch"
    This reverts commit e72079e78004725b8c228338284ca0b3a45ff8ad.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@vite-pwa/nuxt',
     '@vant/nuxt',
+    '@nuxtjs/i18n',
   ],
   runtimeConfig: {
     public: {
@@ -23,10 +24,34 @@ export default defineNuxtConfig({
   },
 
   experimental: {
+    // when using generate, payload js assets included in sw precache manifest
+    // but missing on offline, disabling extraction it until fixed
     payloadExtraction: false,
     inlineSSRStyles: false,
     renderJsonPayloads: true,
     typedPages: true,
+  },
+  i18n: {
+    langDir: 'locales/',
+    vueI18n: './nuxt-i18n.ts',
+    lazy: true,
+    locales: [
+      {
+        code: 'zh-CN',
+        iso: 'zh-CN',
+        name: '简体中文',
+        file: 'cn.json',
+      },
+      {
+        code: 'en',
+        iso: 'en-US',
+        name: 'English',
+        file: 'en.json',
+      },
+    ],
+    defaultLocale: 'zh-CN',
+    strategy: 'prefix_except_default',
+    detectBrowserLanguage: false,
   },
 
   css: ['@unocss/reset/tailwind.css', '@/style/global.scss'],
